878a72cd359ebb9edfbc17536181abc096a24cf0
lyf_laptop <luoyifan902008@126.com>
2025-04-08 11:30:41 +0800

    修改代码结构

---
+++ setup_laptop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Setup unitree g1 in ros2 environment on laptop"
+# source /opt/ros/humble/setup.bash
+# source $HOME/dog_robot/unitree/unitree_ros2/cyclonedds_ws/install/setup.bash
+source $HOME/dog_robot/unitree_go2_ros2_demo/cyclonedds_ws/install/local_setup.bash
+source $HOME/human_robot/unitree_g1_ros2_demo/unitree_g1_ros2_function_pkg/install/local_setup.bash
+
+
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+
+# LAN
+# export CYCLONEDDS_URI='<CycloneDDS><Domain><General><Interfaces>
+#                             <NetworkInterface name="enp0s31f6" priority="default" multicast="default" />
+#                         </Interfaces></General></Domain></CycloneDDS>'
+
+# WLAN
+export CYCLONEDDS_URI='<CycloneDDS><Domain><General><Interfaces>
+                            <NetworkInterface name="wlp0s20f3" priority="default" multicast="default" />
+                        </Interfaces></General></Domain></CycloneDDS>'
