48e36e266c942e66fe6cfaaeb9f3722129e97d75
lyf_laptop <luoyifan902008@126.com>
2025-04-14 09:39:46 +0800

    g1仿真模型导入

--- unitree_g1_ros2_function_pkg/src/g1_sim/launch/g1_urdf2tf.launch.py
+++ unitree_g1_ros2_function_pkg/src/g1_sim/launch/g1_urdf2tf.launch.py
@@ -1,7 +1,7 @@
 import launch
 import launch_ros
 from ament_index_python.packages import get_package_share_directory
-
+import os
 
 def generate_launch_description():
     # 获取默认路径
@@ -11,6 +11,11 @@ def generate_launch_description():
     action_declare_arg_mode_path = launch.actions.DeclareLaunchArgument(
         name='model', default_value=str(g1_model_path),
         description='URDF 的绝对路径')
+    
+    # rviz文件路径
+    rviz_config_dir = os.path.join(
+        urdf_tutorial_path, 'rviz', 'g1_rviz.rviz')
+    
     # 获取文件内容生成新的参数
     robot_description = launch_ros.parameter_descriptions.ParameterValue(
         launch.substitutions.Command(
@@ -27,7 +32,21 @@ def generate_launch_description():
         package='joint_state_publisher',
         executable='joint_state_publisher',
     )
+
+    # RViz2节点
+    rviz_node = launch_ros.actions.Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=[
+                '-d', rviz_config_dir  # 设置指定rviz文件路径
+                ],
+            parameters=[{'use_sim_time': True}],
+            output='screen',
+        )
     return launch.LaunchDescription([
-        action_declare_arg_mode_path, joint_state_publisher_node,
+        action_declare_arg_mode_path, 
+        joint_state_publisher_node,
         robot_state_publisher_node,
+        rviz_node,
     ])
